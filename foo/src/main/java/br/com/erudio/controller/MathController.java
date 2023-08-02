e4af6e3ab3c6ca30a4397177a70e09d07feb785a
Eduardo Brigato <edubrigato@gmail.com>
2023-08-02 20:45:16 -0300

    "Implementando as anotações Autowired e Service"

--- foo/src/main/java/br/com/erudio/controller/MathController.java
+++ foo/src/main/java/br/com/erudio/controller/MathController.java
@@ -2,6 +2,7 @@ package br.com.erudio.controller;
 
 import br.com.erudio.math.SimpleMath;
 import br.com.erudio.request.converters.NumberConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 
-    private final SimpleMath math = new SimpleMath();
+    @Autowired
+    private SimpleMath math;
     @RequestMapping(value = "sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
         if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
