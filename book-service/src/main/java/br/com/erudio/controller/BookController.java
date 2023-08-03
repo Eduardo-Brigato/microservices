df72b233cc78dd6a193a68b57597c1a26b151d98
Eduardo Brigato <edubrigato@gmail.com>
2023-08-03 17:31:57 -0300

    "API Rest Implementation"

--- book-service/src/main/java/br/com/erudio/controller/BookController.java
+++ book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -38,7 +38,7 @@ public class BookController {
         var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 
         var port = environment.getProperty("local.server.port");
-        book.setEnviroment(port);
+        book.setEnviroment("Book port: " + port + " Cambio Port: " + cambio.getEnviroment());
         book.setPrice(cambio.getConvertedValue());
         return book;
     }
